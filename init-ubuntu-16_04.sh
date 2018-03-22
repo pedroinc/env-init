fa5c515478bb6b10538e92910fbb59668a1c9c0c
Pedro Soares <pedroinc@gmail.com>
2018-03-22 17:45:31 -0300

    uncommented lines

--- init-ubuntu-16_04.sh
+++ init-ubuntu-16_04.sh
@@ -1,21 +1,21 @@
-# # install vim
-# sudo apt-get install vim
-#
-# # install curl
-# sudo apt install curl
-#
-# # install atom
-# curl -L https://packagecloud.io/AtomEditor/atom/gpgkey | sudo apt-key add -
-# sudo sh -c 'echo "deb [arch=amd64] https://packagecloud.io/AtomEditor/atom/any/ any main" > /etc/apt/sources.list.d/atom.list'
-# sudo apt-get update
-# sudo apt-get install atom
-#
-# # install .net core
-# curl https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > microsoft.gpg
-# sudo mv microsoft.gpg /etc/apt/trusted.gpg.d/microsoft.gpg
-# sudo sh -c 'echo "deb [arch=amd64] https://packages.microsoft.com/repos/microsoft-ubuntu-xenial-prod xenial main" > /etc/apt/sources.list.d/dotnetdev.list'
-# sudo apt-get update
-# sudo apt-get install dotnet-sdk-2.1.4
+# install vim
+sudo apt-get install vim
+
+# install curl
+sudo apt install curl
+
+# install atom
+curl -L https://packagecloud.io/AtomEditor/atom/gpgkey | sudo apt-key add -
+sudo sh -c 'echo "deb [arch=amd64] https://packagecloud.io/AtomEditor/atom/any/ any main" > /etc/apt/sources.list.d/atom.list'
+sudo apt-get update
+sudo apt-get install atom
+
+# install .net core
+curl https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > microsoft.gpg
+sudo mv microsoft.gpg /etc/apt/trusted.gpg.d/microsoft.gpg
+sudo sh -c 'echo "deb [arch=amd64] https://packages.microsoft.com/repos/microsoft-ubuntu-xenial-prod xenial main" > /etc/apt/sources.list.d/dotnetdev.list'
+sudo apt-get update
+sudo apt-get install dotnet-sdk-2.1.4
 
 # SPOTIFY
 # 1. Add the Spotify repository signing keys to be able to verify downloaded packages
@@ -24,10 +24,10 @@ sudo add-apt-repository "deb http://repository.spotify.com stable non-free" | su
 sudo apt-get update
 sudo apt-get install spotify-client
 
-# sudo add-apt-repository "deb http://repository.spotify.com stable non-free"
-# sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 94558F59
-# sudo apt-get update
-# sudo apt-get install spotify-client spotify-client-qt
+sudo add-apt-repository "deb http://repository.spotify.com stable non-free"
+sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 94558F59
+sudo apt-get update
+sudo apt-get install spotify-client spotify-client-qt
 
 # VSCODE
 # install repository and key:
@@ -35,10 +35,21 @@ curl https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > microso
 sudo mv microsoft.gpg /etc/apt/trusted.gpg.d/microsoft.gpg
 sudo sh -c 'echo "deb [arch=amd64] https://packages.microsoft.com/repos/vscode stable main" > /etc/apt/sources.list.d/vscode.list'
 
-# update package cache and install vscode
+# update repo and install vscode
 sudo apt-get update
 sudo apt-get install code
 
-# jetbrains app tools
-# pycharm
-# tool skype for business like
+# SKY
+# 1. Ensure APT works with HTTPS and up-to-date CA certificates are installed:
+sudo apt-get install apt-transport-https ca-certificates
+# 2. Add appropriate TEL.RED repository to APT sources list:
+sudo sh -c 'echo deb https://tel.red/repos/ubuntu xenial non-free \ > /etc/apt/sources.list.d/telred.list'
+# 3. Download and register TEL.RED software signing public key:
+sudo apt-key adv \ --keyserver hkp://keyserver.ubuntu.com:80 \ --recv-keys 9454C19A66B920C83DDF696E07C8CCAFCE49F8C5
+# 4. update repo
+sudo apt-get update
+sudo apt-get install sky
+
+
+# JETBRAINS APP TOOL
+# PYCHARM
