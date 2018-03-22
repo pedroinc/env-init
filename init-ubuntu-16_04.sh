12df57b872102d8b1d7f4457eb9f301e57e45e40
Pedro Soares <pedroinc@gmail.com>
2018-03-22 19:59:27 -0300

    commented lines for installing second spotify method

--- init-ubuntu-16_04.sh
+++ init-ubuntu-16_04.sh
@@ -24,10 +24,10 @@ sudo add-apt-repository "deb http://repository.spotify.com stable non-free" | su
 sudo apt-get update
 sudo apt-get install spotify-client
 
-sudo add-apt-repository "deb http://repository.spotify.com stable non-free"
-sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 94558F59
-sudo apt-get update
-sudo apt-get install spotify-client spotify-client-qt
+# sudo add-apt-repository "deb http://repository.spotify.com stable non-free"
+# sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 94558F59
+# sudo apt-get update
+# sudo apt-get install spotify-client spotify-client-qt
 
 # VSCODE
 # install repository and key:
